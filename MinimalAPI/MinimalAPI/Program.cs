acab32bad9c7fd40d4c4f3cc1b59b3692b86c579
andreyjpg99 <andreyjpg61@gmail.com>
2025-10-18 21:58:09 -0600

    add mvc  and minimal project

--- MinimalAPI/MinimalAPI/Program.cs
+++ MinimalAPI/MinimalAPI/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Register your DbContext with the connection string
+builder.Services.AddDbContext<ITSupportDbContext>(options =>
+    options.UseSqlServer(connectionString));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.MapGet("/tasks", async (ITSupportDbContext db) =>
+    await db.Tickets.ToListAsync());
 
 app.Run();
